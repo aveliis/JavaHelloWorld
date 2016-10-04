6768b9bae6b3a5ed6320f85478b41e8c1b84843c
aveliis <aveliissaluveer@gmail.com>
2016-10-04 19:57:09 +0300

    Hello World

---
+++ src/Main/Main.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+        System.out.println("Autor: Ave-Liis");
+        System.out.println("Aasta: 2016");
+    }
+}
