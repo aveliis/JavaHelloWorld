25a6270b57f314588c3d5f917ef469d494036432
aveliis <aveliissaluveer@gmail.com>
2016-10-04 20:05:09 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello World!");
         System.out.println("Autor: Ave-Liis Saluveer");
-        System.out.println("Aasta: 2016");
+        System.out.println("Aasta on 2016");
     }
 }
